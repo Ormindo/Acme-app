bd1ace7d75c31bfff05504d932ae074ae85f5974
Ormindo <p3d0c4rl0s@gmail.com>
2024-12-31 00:59:37 -0300

    Conclído até o capítulo 14

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,4 +1,5 @@
 'use server';
+
 import { sql } from '@vercel/postgres';
 import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
@@ -6,48 +7,95 @@ import { redirect } from 'next/navigation';
 
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({
+        invalid_type_error: 'Selecioner um cliente.',
+    }),
+    amount: z.coerce
+        .number()
+        .gt(0, { message: 'Insira um valor superior a R$ 0.' }),
+    status: z.enum(['pending', 'paid'], {
+        invalid_type_error: 'Selecione o status de fatura.'
+    }),
     date: z.string(),
 });
 
-const CreateInvoice = FormSchema.omit({ id: true, date: true});
-
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
-    const { customerId, amount, status } = CreateInvoice.parse({
+export type State = {
+    errors?: {
+        customerId?: string[];
+        amount?: string[];
+        status?: string[];
+    };
+    message?: string | null;
+}
+
+export async function createInvoice(prevState: State, formData: FormData) {
+    const validatedFields = CreateInvoice.safeParse({
       customerId: formData.get('customerId'),
       amount: formData.get('amount'),
       status: formData.get('status'),
     });
+
+    if(!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Campos pendente. Falha ao criar fatura.',
+        };
+    }
+
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
-    await sql`
-        INSERT INTO invoices (customer_id, amount, status, date)
-        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-    `;
+    try {
+        await sql`
+            INSERT INTO invoices (customer_id, amount, status, date)
+            VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+        `;
+    } catch (error) {
+        return{
+            message: 'Erro no Banco de Dados: Falha ao criar faturas.',
+        }
+    }
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
 
-export async function updateInvoice(id: string, formData: FormData) {
-    const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(
+    id: string,
+    prevState: State,
+    formData: FormData
+) {
+    const validatedFields = UpdateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
 
+    if(!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Campos pendente. Falha ao atualizar a fatura.'
+        }
+    }
+
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
 
+    try {
     await sql`
         UPDATE invoices
         SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
         WHERE id = ${id}
     `;
+    } catch (error) {
+        return {
+            message: 'Erro no Banco de Dados não foi possível atualizar a fatura.',
+        }
+    }
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
@@ -55,9 +103,18 @@ export async function updateInvoice(id: string, formData: FormData) {
 
 export async function deleteInvoice(id: string) {
 
-    await sql`
-        DELETE FROM invoices WHERE id = ${id}
-    `;
+    try {
+        await sql`
+            DELETE FROM invoices WHERE id = ${id}
+        `;
+        return { 
+            message: 'Fatura excluida.'
+        }
+    } catch (error) {
+        return {
+            message: 'Erro no Banco de Dados, não foi possivel deletar a fatura.',
+        }
+    }
 
     revalidatePath('/dashboard/invoices');
 }
