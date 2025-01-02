8f2c132da741e98159fa964724a92ffcea6f795d
Ormindo <p3d0c4rl0s@gmail.com>
2025-01-02 19:48:07 -0300

    Concluído até o capítulo 16

--- app/dashboard/(overview)/loading.tsx
+++ app/dashboard/(overview)/loading.tsx
@@ -1,4 +1,9 @@
 import DashboardSkeleton from "../../ui/skeletons"
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: 'Dashboard',
+  };
 
 export default function Page() {
     return <DashboardSkeleton />
